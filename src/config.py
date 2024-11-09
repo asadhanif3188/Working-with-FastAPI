643bd7192fed92fe9b1df6a336d4287baea225d3
Asad Hanif <asadhanif3188@gmail.com>
2024-11-09 07:23:49 +0500

    config.py file added

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    # app_name: str = "Bookly APIs"
+    # admin_email: str
+    # items_per_user: int = 50
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        extra="ignore",
+        env_file_encoding="utf-8"
+        )
+
+
+ss = Settings()
+print(ss.DATABASE_URL)
