7d709af24a80139f34280561ce9d9f41d61a3c5e
Asad Hanif <asadhanif3188@gmail.com>
2024-11-09 11:53:18 +0500

    book table creation code added

--- src/db/main.py
+++ src/db/main.py
@@ -1,6 +1,7 @@
-from sqlmodel import create_engine, text
+from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
+
 
 engine = AsyncEngine(
     create_engine(
@@ -14,6 +15,12 @@ engine = AsyncEngine(
 
 async def init_db():
     async with engine.begin() as conn:
-        statment = text("Select 'hello';")
-        result = await conn.execute(statment)
-        print(result.all())
+        # *** these statements are just for testing**
+        # statment = text("Select 'hello';")
+        # result = await conn.execute(statment)
+        # print(result.all())
+        # ******************************************
+        from src.books.models import Book
+
+        await conn.run_sync(SQLModel.metadata.create_all)
+
